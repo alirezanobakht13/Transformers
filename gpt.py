503219d2646a0e97b07563cdba6fc7a4407ca871
alirezanobakht13 <a.nobakht13@gmail.com>
2023-10-10 18:09:48 +0330

    Change directory structure

--- gpt.py
+++ gpt.py
@@ -1,1 +1,16 @@
+import random
 
+import torch
+import torch.nn as nn
+
+
+def set_seed(seed: int):
+    """set seed for reproducible result
+
+    Args:
+        seed (int): seed
+    """
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
